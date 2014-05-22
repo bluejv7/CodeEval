ab308c81385b50e3fd4540f4c3e746edf466f071
bluejv <bluejv7@gmail.com>
2014-05-21 22:40:14 -0700

    Updated templates; added solution to swap elements

--- SwapElements/main.cpp
+++ SwapElements/main.cpp
@@ -2,10 +2,12 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <cstdlib>
 
 using namespace std;
 
 vector<string> split(char, string);
+void swap_elements(string);
 
 int main(int argc, char** argv)
 {
@@ -21,11 +23,54 @@ int main(int argc, char** argv)
         }
         else 
         {
-            // Do something here
+            swap_elements(lineBuffer);
         }
     }
 
     return 0;
+}
+
+void swap_elements(string line)
+{
+    vector<string> args = split(' ', line);
+    vector<int> numbers;
+    size_t colon_index = 0;
+
+    // Put all the elements into a vector
+    for (size_t i = 0; i < args.size(); i++)
+    {
+        if (args[i] == ":")
+        {
+            colon_index = i;
+            break;
+        }
+        numbers.push_back(atoi(args[i].c_str()));
+    }
+
+    // swap elements
+    for (size_t i = colon_index + 1; i < args.size(); i++)
+    {
+        string current_arg = args[i];
+        // if there is a comma at the end, parse it out
+        if (current_arg[current_arg.size()-1] == ',')
+        {
+            current_arg = current_arg.substr(0, current_arg.size()-1);
+        }
+        
+        vector<string> swap = split('-', args[i]);
+        size_t first_index = atoi(swap[0].c_str());
+        size_t second_index = atoi(swap[1].c_str());
+        int temp_value = numbers[first_index];
+        numbers[first_index] = numbers[second_index];
+        numbers[second_index] = temp_value;
+    }
+
+    // Now print!  =)
+    for (size_t i = 0; i < numbers.size(); i++)
+    {
+        cout << numbers[i] << " ";
+    }
+    cout << endl;
 }
 
 vector<string> split(char delim, string s)
